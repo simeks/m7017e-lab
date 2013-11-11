013ae732677831f04cc8f09abf656e6b159ab25b
Simon <simon.ekstrom@gmail.com>
2013-11-11 21:28:18 +0100

    Changed from GTK to Qt.

--- tundra.lua
+++ tundra.lua
@@ -1,8 +1,10 @@
 require "tundra.syntax.glob"
 native = require "tundra.native"
 
+
 local win32_config = {
 	Env = {
+		QT5 = native.getenv("QT5"),
 		GSTREAMER_BASEPATH = native.getenv("GSTREAMER_SDK_ROOT_X86", "."),
 		GSTREAMER_INCLUDE = "$(GSTREAMER_BASEPATH)/include/",
 		GSTREAMER_LIBS = "$(GSTREAMER_BASEPATH)/lib/",
@@ -23,14 +25,20 @@ local win32_config = {
 
 local macosx_config = {
 	Env = {
+		QT5 = native.getenv("QT5"),
 		GSTREAMER_INCLUDE = "/Library/Frameworks/GStreamer.framework/Versions/0.10/Headers",
 		GSTREAMER_LIBS = "/Library/Frameworks/GStreamer.framework/Versions/0.10/lib",
 		CXXOPTS = {
-			"-Werror", "-Wall", "-std=c++11",
+			"-Wall", "-std=c++11", "-stdlib=libc++",
 			{ "-O0", "-g"; Config = "*-*-debug" },
 			{ "-O2"; Config = {"*-*-release", "*-*-production"} },
 		},
-		LD = { "-lc++" },
+		COPTS = {
+			"-Wall",
+			{ "-O0", "-g"; Config = "*-*-debug" },
+			{ "-O2"; Config = {"*-*-release", "*-*-production"} },
+		},
+		LD = { "-lc++", "-F$(QT5)/lib", },
 	}
 }
 
@@ -77,89 +85,10 @@ Build {
 		},
 	},
 
-	Units = function ()
-		Program {
-			Name = "Main",
-			Sources = {
-				FGlob {
-					Dir = "src",
-					Extensions = { ".cpp", ".h", ".inl" },
-					Filters = {
-						{ Pattern = "win32"; Config = "win32-*-*"; },
-					},
-				},
-			},
-			Env = {
-				CPPPATH = { 
-					"$(GSTREAMER_INCLUDE)",
-					"$(GSTREAMER_INCLUDE)",
-					"$(GSTREAMER_INCLUDE)/atk-1.0",
-					"$(GSTREAMER_INCLUDE)/cairo",
-					"$(GSTREAMER_INCLUDE)/clutter-1.0",
-					"$(GSTREAMER_INCLUDE)/cogl",
-					"$(GSTREAMER_INCLUDE)/FLAC",
-					"$(GSTREAMER_INCLUDE)/fontconfig",
-					"$(GSTREAMER_INCLUDE)/freetype2",
-					"$(GSTREAMER_INCLUDE)/gail-1.0",
-					"$(GSTREAMER_INCLUDE)/gdk-pixbuf-2.0",
-					"$(GSTREAMER_INCLUDE)/gio-win32-2.0",
-					"$(GSTREAMER_INCLUDE)/glib-2.0",
-					"$(GSTREAMER_INCLUDE)/gnutls",
-					"$(GSTREAMER_INCLUDE)/gstreamer-0.10",
-					"$(GSTREAMER_INCLUDE)/gtk-2.0",
-					"$(GSTREAMER_INCLUDE)/jasper",
-					"$(GSTREAMER_INCLUDE)/json-glib-1.0",
-					"$(GSTREAMER_INCLUDE)/kate",
-					"$(GSTREAMER_INCLUDE)/libcroco-0.6",
-					"$(GSTREAMER_INCLUDE)/libdv",
-					"$(GSTREAMER_INCLUDE)/libpng15",
-					"$(GSTREAMER_INCLUDE)/librsvg-2.0",
-					"$(GSTREAMER_INCLUDE)/libsoup-2.4",
-					"$(GSTREAMER_INCLUDE)/libvisual-0.4",
-					"$(GSTREAMER_INCLUDE)/libxml2",
-					"$(GSTREAMER_INCLUDE)/nettle",
-					"$(GSTREAMER_INCLUDE)/ogg",
-					"$(GSTREAMER_INCLUDE)/opus",
-					"$(GSTREAMER_INCLUDE)/orc-0.4",
-					"$(GSTREAMER_INCLUDE)/pango-1.0",
-					"$(GSTREAMER_INCLUDE)/pixman-1",
-					"$(GSTREAMER_INCLUDE)/pycairo",
-					"$(GSTREAMER_INCLUDE)/pygtk-2.0",
-					"$(GSTREAMER_INCLUDE)/schroedinger-1.0",
-					"$(GSTREAMER_INCLUDE)/soundtouch",
-					"$(GSTREAMER_INCLUDE)/speex",
-					"$(GSTREAMER_INCLUDE)/taglib",
-					"$(GSTREAMER_INCLUDE)/theora",
-					"$(GSTREAMER_INCLUDE)/vorbis",
-					"$(GSTREAMER_INCLUDE)/vpx",
-					"$(GSTREAMER_INCLUDE)/wavpack",
-					
-					"$(GSTREAMER_LIBS)/glib-2.0/include",
-					"$(GSTREAMER_LIBS)/gtk-2.0/include",
-					"$(GSTREAMER_LIBS)/libffi-3.0.11/include",
-				},
-				LIBPATH = {
-					"$(GSTREAMER_LIBS)",
-				},
-				LIBS = {
-				},
-			},
-			Libs = { 
-				{ "kernel32.lib", "user32.lib", "gdi32.lib", "comdlg32.lib", "advapi32.lib", 
-					"gstreamer-0.10.lib", 
-					"glib-2.0.lib", 
-					"gtk-win32-2.0.lib", 
-					"gdk-win32-2.0.lib", 
-					"cairo.lib", 
-					"gstapp-0.10.lib", 
-					"gstbase-0.10.lib";
-					"gstinterfaces-0.10.lib",
-					"gobject-2.0.lib"; Config = { "win32-*-*", "win64-*-*" } },
-				{ "gstreamer-0.10"; Config = { "macosx-*-*", "macosx-*-*" } }
-			},
+	Passes = {
+		GenerateSources = { Name="GenerateSources", BuildOrder = 1 },
+	},
 
-		}
-		Default "Main"
-	end,
+	Units = "units.lua"
 }
 
---
+++ units.lua
@@ -0,0 +1,100 @@
+
+
+DefRule {
+	Name = "GenerateMoc",
+	Pass = "GenerateSources",
+	Command = "$(QT5)/bin/moc $(<) -o $(@)",
+
+	Blueprint = {
+		Input = { Required = true, Type = "string" },
+		Output = { Required = true, Type = "string" },
+	},
+
+	Setup = function (env, data)
+		return {
+			InputFiles    = { data.Input },
+			OutputFiles   = { "$(OBJECTDIR)/_generated/" .. data.Output },
+		}
+	end,
+}
+
+DefRule {
+	Name = "GenerateQRC",
+	Pass = "GenerateSources",
+	Command = "$(QT5)/bin/rcc $(<) -o $(@) -name application",
+
+	Blueprint = {
+		Input = { Required = true, Type = "string" },
+		Output = { Required = true, Type = "string" },
+	},
+
+	Setup = function (env, data)
+		return {
+			InputFiles    = { data.Input },
+			OutputFiles   = { "$(OBJECTDIR)/_generated/" .. data.Output },
+		}
+	end,
+}
+
+
+
+
+local function GenerateMocSources(sources)
+	local result = {}
+	for _, src in ipairs(tundra.util.flatten(sources)) do
+		result[#result + 1] = GenerateMoc { Input = src, Output = tundra.path.get_filename_base(src) .. "_moc.cpp" }
+	end
+	return result
+end
+
+
+Program {
+	Name = "Main",
+	Sources = {
+		FGlob {
+			Dir = "src",
+			Extensions = { ".c", ".cpp", ".h", ".inl" },
+			Filters = {
+				{ Pattern = "win32"; Config = "win32-*-*"; },
+			},
+		},
+		GenerateMocSources {
+			Glob { 
+				Dir = "src", 
+				Extensions = { ".h" } 
+			}, 
+		},
+		GenerateQRC {
+			Input = "data/application.qrc",
+			Output = "application.cpp"
+		}
+	},
+	Env = {
+		CPPPATH = { 
+			"$(GSTREAMER_INCLUDE)",
+			"$(GSTREAMER_INCLUDE)/gstreamer-0.10",
+			"$(QT5)/include/QtWidgets",
+			"$(QT5)/include/QtGui",
+			"$(QT5)/include/QtCore", 
+			"$(QT5)/include",	
+		},
+		LIBPATH = {
+			"$(GSTREAMER_LIBS)",
+			"$(QT5)/lib",
+		},
+	},
+	Libs = { 
+		{ 	"kernel32.lib", "user32.lib", "gdi32.lib", "comdlg32.lib", "advapi32.lib", 
+			"gstreamer-0.10.lib"; 
+			Config = { "win32-*-*" } 
+		},
+		{ 	"gstreamer-0.10"; 
+			Config = { "macosx-*-*", "macosx-*-*" } 
+		}
+	},
+
+	Frameworks = { "Cocoa", "QtCore", "QtWidgets", "QtGui", "OpenGL", "AGL"  },
+
+}
+Default "Main"
+
